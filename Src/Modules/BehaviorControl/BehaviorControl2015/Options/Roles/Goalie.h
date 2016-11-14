2159df2b8f2c7816b1524c938ee02cd41102f725
PeeM <piotrmk1@gmail.com>
2016-11-14 19:17:58 +0000

    After noticing a ball, the robot will move into tracking phase.

--- Src/Modules/BehaviorControl/BehaviorControl2015/Options/Roles/Goalie.h
+++ Src/Modules/BehaviorControl/BehaviorControl2015/Options/Roles/Goalie.h
@@ -1,8 +1,79 @@
-
 /** A test goalie option without common decision */
 option(Goalie)
 {
-  initial_state(start)
+
+initial_state(start)
+  {
+    transition
+    {
+      if(state_time > 1000)
+        goto lookAround;
+    }
+    action
+    {
+      theHeadControlMode = HeadControl::lookForward;
+      Stand();
+    }
+  }
+
+state(lookAround)
+  {
+    transition
+    {
+        if(libCodeRelease.timeSinceBallWasSeen() < 300)
+        goto trackTheBall;
+    }
+    action
+    {
+      theHeadControlMode = HeadControl::lookLeftAndRight;
+    }
+  }
+
+state(trackTheBall)
+  {
+    int shotDetected = 0;
+
+    transition
+    {
+      if(libCodeRelease.timeSinceBallWasSeen() > 7000)
+        goto lookAround;
+
+      if(shotDetected)
+        goto defendTheShot;
+    }
+    action
+    {
+        float op = theBallModel.estimate.position.norm();
+        float np = op;
+
+        do
+        {
+            op = np;
+            np = theBallModel.estimate.position.norm();
+        }
+        while (op-np < 1.0f);
+
+        shotDetected = 1;
+    }
+  }
+
+state(defendTheShot)
+  {
+//TODO: REMOVE AND REWRITE
+    transition
+    {
+        if(libCodeRelease.timeSinceBallWasSeen() < 300)
+        goto trackTheBall;
+    }
+    action
+    {
+      theHeadControlMode = HeadControl::lookForward;
+    }
+  }
+
+/* ODTAD
+
+initial_state(start)
   {
     transition
     {
@@ -19,32 +90,54 @@ option(Goalie)
   state(detectShot)
   {
     transition
-    {
+    {*/
        /* if (shotDetected) {
           goto saveShot;
        }
        */
+        /*if(libCodeRelease.timeSinceBallWasSeen() < 300)
+        goto trackTheBall;
     }
-    action 
+    action
     {
       theHeadControlMode = HeadControl::lookLeftAndRight;
       // process frame
       // determine if the shot was shot
     }
   }
+
+DOTAD MIELISMY
+*/
+
 /*
-  state(saveShot) 
+  state(trackTheBall)
   {
-    action 
+    transition
+    {
+      if(libCodeRelease.timeSinceBallWasSeen() > 7000)
+        goto turnToBall;
+    }
+    action
+    {
+      theHeadControlMode = HeadControl::lookForward;
+      WalkAtSpeedPercentage(Pose2f(1.f, 0.f, 0.f));
+    }
+  }
+
+
+
+  state(saveShot)
+  {
+    action
     {
       // lay down the goalie
       // if (direction == DIR_LEFT) {
      */ /*
-	// walk to the left
-      	WalkToTarget(Pose2f(50.f, 50.f, 50.f), Pose2f(theBallModel.estimate.position.angle(), 0.f, 0.f));
+        // walk to the left
+        WalkToTarget(Pose2f(50.f, 50.f, 50.f), Pose2f(theBallModel.estimate.position.angle(), 0.f, 0.f));
          } else {
-	// walk to the right
-      	WalkToTarget(Pose2f(50.f, 50.f, 50.f), Pose2f(theBallModel.estimate.position.angle(), 0.f, 0.f));
+        // walk to the right
+        WalkToTarget(Pose2f(50.f, 50.f, 50.f), Pose2f(theBallModel.estimate.position.angle(), 0.f, 0.f));
          }
 */ /*
     }
@@ -113,5 +206,6 @@ option(Goalie)
       theHeadControlMode = HeadControl::lookForward;
       InWalkKick(WalkRequest::left, Pose2f(libCodeRelease.angleToGoal, theBallModel.estimate.position.x() - 160.f, theBallModel.estimate.position.y() - 55.f));
     }
-  } */ 
+  } */
 }
+
