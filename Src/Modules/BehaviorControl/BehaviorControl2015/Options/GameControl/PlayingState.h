9a5e0ba02e29b24a2e58ab39a55d5a124b4aac12
Lrakulka <olecsandr32@gmail.com>
2016-11-21 10:15:03 +0100

    Chose role action improved

--- Src/Modules/BehaviorControl/BehaviorControl2015/Options/GameControl/PlayingState.h
+++ Src/Modules/BehaviorControl/BehaviorControl2015/Options/GameControl/PlayingState.h
@@ -2,103 +2,15 @@ option(PlayingState)
 {
   initial_state(play)
   {
-    transition
-    {
-      if(state_time > 1000)
-        goto decision_state;
-    }
+    
     action
     {
-      theHeadControlMode = HeadControl::lookForward;
-      Stand();
-    }
-  }
-      
-  state(decision_state)
-  {
-  	  transition
-	  {
-	      if(libCodeRelease.timeSinceBallWasSeen() > 7000)
-	        goto searchForBall;
-	  }
-      action
-      {
-	      if(theBallModel.estimate.position.norm() < 1200.f) {
-	          Striker();
+    	if (theRobotInfo.number != 1) {
+    		 Striker();
 	      } else {
 	      	   Goalie();
 	      }
-      }
-  }
-  
-  state(searchForBall)
-  {
-    transition
-    {
-      if(libCodeRelease.timeSinceBallWasSeen() < 300)
-        goto decision_state;
     }
-    action
-    {
-      theHeadControlMode = HeadControl::lookForward;    
-      WalkAtSpeedPercentage(Pose2f(1.f, 0.f, 0.f));
-    }
-  }
-  
-  
-  state(half_turn_1)
-  {
-      transition
-      {
-          if (state_time > 6000)
-             goto half_turn_2;
-      }
-      action
-      {
-    	  SpecialAction(SpecialActionRequest::testAnimation);
-          //WalkToTarget(Pose2f(100.f, 100.f, 100.f), Pose2f(3.142f, 0.f, 0.f));
-      }
-  }
-
-
-  state(half_turn_2)
-  {
-      transition
-      {
-           if (state_time > 6000)
-               goto half_turn_3;
-      }
-      action
-      {
-          WalkToTarget(Pose2f(100.f, 100.f, 100.f), Pose2f(3.143f, 0.f, 0.f));
-      }
-  }
-
-  state(half_turn_3)
-  {
-      transition
-      {
-           if (state_time > 6000)
-               goto half_turn_4;
-      }
-      action
-      {
-           WalkToTarget(Pose2f(100.f, 100.f, 100.f), Pose2f(3.144f, 0.f, 0.f));
-      }
-  }
-
-  state(half_turn_4)
-  {
-      transition
-      {
-           if (state_time > 6000)
-               goto decision_state;
-      }
-      action
-      {
-           WalkToTarget(Pose2f(100.f, 100.f, 100.f), Pose2f(3.145f, 0.f, 0.f));
-      }
-  }
-  
+  }  
 }
 
