48a0907b480c34966d15e8ee6007576d09bdabdf
Jacek Lebioda <jacek.lebioda@student.put.poznan.pl>
2016-12-01 01:57:32 +0100

    Cleanups and documentation

--- Src/Modules/BehaviorControl/BehaviorControl2015/Options/HeadControl/LookLeftAndRight.h
+++ Src/Modules/BehaviorControl/BehaviorControl2015/Options/HeadControl/LookLeftAndRight.h
@@ -1,17 +1,21 @@
-/** Pun intended. Looks straight ahead in a way that the Nao V4's cameras cover the area in front of its feet as well as everything else in front of the robot.*/
+/**
+ * Pun intended. Looks straight ahead in a way that the Nao V4's cameras
+ * cover the area in front of its feet as well as everything
+ * else in front of the robot.
+ */
 option(LookLeftAndRight, (float) (0.2f) tilt)
 {
   initial_state(LookLeftAndRight)
   {
     transition
     {
-	if (state_time > 1000) {
-		goto lookLeft;
-        }
+	     if (state_time > 1000) { // After one second
+		     goto lookLeft; // Robot starts looking left
+       }
     }
     action
     {
-      SetHeadPanTilt(0.f, tilt, 150_deg);
+      SetHeadPanTilt(0.f, tilt, 150_deg); // Look straight
     }
   }
 
@@ -19,13 +23,13 @@ option(LookLeftAndRight, (float) (0.2f) tilt)
   {
     transition
     {
-	if (state_time > 1000) {
-		goto lookRight;
-	}
+    	if (state_time > 1000) { // After one second of looking left movement
+    		goto lookRight; // Request looking right motion
+    	}
     }
     action
     {
-      SetHeadPanTilt(1.0f, tilt, 150_deg);
+      SetHeadPanTilt(1.0f, tilt, 150_deg); // Look to the left
     }
   }
 
@@ -33,13 +37,13 @@ option(LookLeftAndRight, (float) (0.2f) tilt)
   {
     transition
     {
-	if (state_time > 1000) {
-		goto lookLeft;
-	}
+    	if (state_time > 1000) { // After one second of looking right movement
+    		goto lookLeft; // Request looking left motion
+    	}
     }
     action
     {
-      SetHeadPanTilt(-1.f, tilt, 150_deg);
+      SetHeadPanTilt(-1.f, tilt, 150_deg); // Look to the right
     }
   }
 }
